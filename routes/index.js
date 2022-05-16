a05f2a14efdbb0faf0cc77bcf203b114b4dde305
msi-a-ou <57803939+was865@users.noreply.github.com>
2022-05-16 11:57:43 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,14 @@
 const https = require("https")
 const express = require("express")
 const app = express()
-const PORT = process.env.PORT || 3000
+var router = express.Router();
 const TOKEN = process.env.LINE_ACCESS_TOKEN
 
-app.use(express.json())
-app.use(express.urlencoded({
-  extended: true
-}))
-
-app.get("/", (req, res) => {
+router.get("/", (req, res) => {
   res.sendStatus(200)
 })
 
-app.post("/webhook", function(req, res) {
+router.post("/webhook", function(req, res) {
   res.send("HTTP POST request sent to the webhook URL!")
   // ユーザーがボットにメッセージを送った場合、返信メッセージを送る
   if (req.body.events[0].type === "message") {
@@ -65,6 +60,6 @@ app.post("/webhook", function(req, res) {
   }
 })
 
-app.listen(PORT, () => {
+router.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`)
 })
