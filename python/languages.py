4fb8bac870f10eb837b91bca46abdbb8c722fba8
msi-a-ou <57803939+was865@users.noreply.github.com>
2022-05-17 17:49:57 +0900

    no japanese

--- python/languages.py
+++ python/languages.py
@@ -8,3 +8,9 @@ class GINZA:
     ISO_639_1 = 'ja_ginza_electra'
     ISO_639 = 'ginza'
     ENGLISH_NAME = 'ginza'
+
+
+class CHI:
+    ISO_639_1 = 'zh'
+    ISO_639 = 'chi'
+    ENGLISH_NAME = 'Chinese'
