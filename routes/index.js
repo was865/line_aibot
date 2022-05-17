9d333312e62558bfdb48d84d58eb6ebc447b0776
msi-a-ou <57803939+was865@users.noreply.github.com>
2022-05-17 16:24:06 +0900

    do

--- routes/index.js
+++ routes/index.js
@@ -43,9 +43,7 @@ function getAnwser(input_text) {
       scriptPath: './'
     }
 
-    console.log()
-
-    var pyshell = new PythonShell('chatbot.py');
+    var pyshell = new PythonShell('./python/chatbot.py', options);
 
     // sends a message to the Python script via stdin
     var data = {
