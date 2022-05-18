0895f885ea12fd54703c59468752dd891ec876f6
was865 <865411640@qq.com>
2022-05-18 16:35:14 +0900

    Update chatbot.py

--- python/chatbot.py
+++ python/chatbot.py
@@ -16,6 +16,7 @@ bot = ChatBot(
     name='ボット',
     tagger_language=languages.JPN  # 統計モデルの言語指定
     # tagger_language=languages.GINZA  # GINZAモデルの場合
+    # ,read_only=True,         # train後対話では学習させない
 )
 
 if json_dict["doTrain"] == True:
