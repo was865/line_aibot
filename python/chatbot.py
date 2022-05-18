47261c4b1c6c58aec8c11cf249d8ee8acac80fc5
msi-a-ou <57803939+was865@users.noreply.github.com>
2022-05-18 12:03:18 +0900

    Update chatbot.py

--- python/chatbot.py
+++ python/chatbot.py
@@ -21,7 +21,7 @@ if json_dict["doTrain"] == True:
     trainer = ChatterBotCorpusTrainer(bot)
 
     trainer.train(
-        './chatterbot_corpus/data/japanese'  # 日本語用コーパス
+        '/chatterbot_corpus/data/japanese'  # 日本語用コーパス
     )
 
 try:
